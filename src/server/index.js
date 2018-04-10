009fbcad4b7fa12ef4cf603ffdb8aee95d24ffd4
Trevor Old <trevor.old@outlook.com>
2018-04-09 22:45:03 -0700

    removed unneccessary libs

--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,39 @@
-const koa = require('koa');
-const app = new koa();
-const router = require('koa-router');
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
 const koaBody = require('koa-body');
-const path = require('path');
+const axios = require('axios');
 const serve = require('koa-static');
+const morgan = require('koa-morgan')
 
 const PORT = process.env.PORT || 8080;
+const ADDRESS = process.env.ADDRESS;
 
-const publicFiles = serve(path.join(__dirname + '../../dist'));
+const publicFiles = serve('dist');
 
 publicFiles._name = 'static /dist';
 
+const app = new Koa();
 
-app.use(serve('dist'))
+const router = new Router();
+
+app.use(morgan('dev'))
+
+app.use(publicFiles);
+
+router.get('/gas', async (ctx, next) => {
+  await axios
+    .get(`https://neoscan.io/api/main_net/v1/get_address/${ADDRESS}`)
+    .then((response) => {
+      ctx.response.body = response.data;
+    })
+    .catch(err => ctx.response.body = err);
+
+})
+
+app.use(router.routes())
 
 
-app.listen(PORT, () => `listening on ${PORT}`);
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
