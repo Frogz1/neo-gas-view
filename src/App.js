5783b7a45ce26b64d6095a9abdc73aac9053a46d
Trevor Old <trevor.old@outlook.com>
2018-04-09 21:56:12 -0700

    bootstrapping of project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      address: ''
+    };
+    
+  }
+
+  render() {
+    <div>
+      <h3>Neo to Gas baby</h3>
+    </div>
+  }
+}
+
+export default ReactDOM.render(<App />, document.getElementById('app'));
