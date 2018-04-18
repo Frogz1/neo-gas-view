6b6a49de2855546bc52639ac0678d8da3faeb958
Trevor Old <trevor.old@outlook.com>
2018-04-18 13:10:57 -0700

    remove unnecessary loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,13 +12,6 @@ module.exports = {
         },
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'eslint-loader',
-        },
-      },
-      {
         test: /\.html$/,
         use: [
           {
