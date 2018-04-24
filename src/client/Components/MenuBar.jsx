6fae3045a78c9700326a2dc9153ad78913267bac
Trevor Old <trevor.old@outlook.com>
2018-04-24 12:47:30 -0600

    adding proptypes to components

--- src/client/Components/MenuBar.jsx
+++ src/client/Components/MenuBar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Input, Menu, Item, Container } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-const MenuBar = ({ neoUsd, gasUsd, blockRate, setView }) => (
+const MenuBar = ({
+  neoUsd, gasUsd, blockRate, setView,
+}) => (
   <Menu style={{ textAlign: 'center' }} stackable>
     <Container>
       <Menu.Item name="wallet" as="a" onClick={() => setView('wallet')} />
@@ -23,4 +26,13 @@ const MenuBar = ({ neoUsd, gasUsd, blockRate, setView }) => (
   </Menu>
 );
 
+MenuBar.propTypes = {
+  neoUsd: PropTypes.number.isRequired,
+  gasUsd: PropTypes.number.isRequired,
+  blockRate: PropTypes.number,
+  setView: PropTypes.string.isRequired,
+};
+MenuBar.defaultProps = {
+  blockRate: 20,
+};
 export default MenuBar;
