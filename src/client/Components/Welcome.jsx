f6018330ce46ddc34adb78ead120ba447a46f633
Trevor Old <trevor.old@outlook.com>
2018-04-24 13:01:16 -0600

    clean up commented sections of  welcome component

--- src/client/Components/Welcome.jsx
+++ src/client/Components/Welcome.jsx
@@ -3,39 +3,30 @@ import { Container, Header, Input, Button, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 const Welcome = ({ setView, wallet, updateAddress }) => (
-  <Container textAlign="center">
-    {/* <Card centered>
-      <Icon as="i" name="calculator" size="massive" fitted />
-      <Card.Content style={{ textAlign: 'center' }}>
-        <Card.Header as="a">
-        Gas calculator
-        </Card.Header>
-        <Card.Meta>
-      See estimated GAS generation for n amount of NEO
-        </Card.Meta>
-      </Card.Content>
-    </Card> */}
-    <Header as="h2" icon>
-      <Image src="/assets/neo-logo.ico" />
-      Welcome!
-      <Header.Subheader>
-        Select wallet to see estimated gas earnings or select calculator to estimate gas earnings for n neo
-      </Header.Subheader>
-    </Header>
-
-
-    <Input
-      style={{ width: '315px', marginRight: '5px' }}
-      size="small"
-      type="text"
-      value={wallet.newAddress}
-      onChange={(e, data) => {
+  <div>
+    <Container textAlign="center">
+      <Header as="h2" icon>
+        <Image src="/assets/neo-logo.ico" />
+        Welcome!
+        <Header.Subheader>
+          Select wallet to see estimated gas earnings or select calculator to estimate gas earnings for n neo
+        </Header.Subheader>
+      </Header>
+    </Container>
+    <Container textAlign="center">
+      <Input
+        style={{ width: '315px', marginRight: '5px' }}
+        size="small"
+        type="text"
+        value={wallet.newAddress}
+        onChange={(e, data) => {
           updateAddress(data.value);
       }}
-    />
-    <Button onClick={() => setView('wallet')} color="green">Wallet</Button>
-    <Button onClick={() => setView('calculator')} color="green">Calculator</Button>
-  </Container>
+      />
+      <Button onClick={() => setView('wallet')} color="green">Wallet</Button>
+      <Button onClick={() => setView('calculator')} color="green">Calculator</Button>
+    </Container>
+  </div>
 );
 
 Welcome.propTypes = {
